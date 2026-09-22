bc937a2f2c07b3039d73330de67ae59a3450a6ae
Doc-Y-tor <youmnamounir9@gmail.com>
2026-09-22 17:04:04 +0100

    added youtube script

---
+++ you-tube.py
@@ -0,0 +1,4 @@
+print("Welcome to your Generator Youtube channel")
+name=input("What's your name?")
+subject=input("What's your channel subject?")
+print("You can name you channel "+"("+ subject +" with " + name+")")
