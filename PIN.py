69ae69343b53a232d6a314242ceaa9dab014aced
Doc-Y-tor <youmnamounir9@gmail.com>
2026-09-24 21:47:15 +0100

    publish to git hub

--- PIN.py
+++ PIN.py
@@ -2,3 +2,11 @@ code=input("Please enter your PIN number\n")
 X=len(code)
 print(f"The code numbers is {X}")
 
+
+X=input("Please enter two digits:\n ")
+print(int(X[0])+ int(X[1]))
+
+
+
+      
+      
